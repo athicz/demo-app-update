f688990b9429339435d956b491db3d3cad729abf
athicz <athichai.lim@gmail.com>
2023-06-12 11:24:14 +0700

    Initial commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { AppUpdate } from '@ionic-native/app-update/ngx';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
@@ -13,6 +15,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     FormsModule,
     IonicModule,
     HomePageRoutingModule
+  ],
+  providers: [
+    AppVersion
+    , AppUpdate
   ],
   declarations: [HomePage]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AppUpdate, AppUpdateOptions } from '@ionic-native/app-update/ngx';
+import { AppVersion } from '@ionic-native/app-version/ngx';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  public versionNumber!:string ;
+  public versionCode!:any ;
+  public versionName!:any ;
+  public versionPkgName!:any ;
+  constructor(private appVersion: AppVersion
+    , private appUpdate: AppUpdate
+    , private alertController: AlertController
+    ) { }
 
-  constructor() {}
+  ngOnInit(): void {
+    this.appVersion.getVersionNumber().then((res) => {
+      this.versionNumber = res;
+      console.log('Version Number:', this.versionNumber);
+    });
+
+    this.appVersion.getVersionCode().then((res) => {
+      this.versionCode = res;
+      console.log('Version Code:', this.versionCode);
+    });
+
+    this.appVersion.getAppName().then((res) => {
+      this.versionName = res;
+      console.log('App Name:', this.versionName);
+    });
+
+    this.appVersion.getPackageName().then((res) => {
+      this.versionPkgName = res;
+      console.log('Package Name:', this.versionPkgName);
+    });
+
+    const updateUrl = 'http://192.168.1.42:8080/files/update.xml'; // Replace with your update URL
+
+    this.showUpdateAlert();
+  }
+
+  async showUpdateAlert() {
+    const alert = await this.alertController.create({
+      header: 'Update Available',
+      buttons: [
+        {
+          text: 'No, thanks',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('User clicked No, thanks');
+          },
+        },
+        {
+          text: 'Update',
+          handler: () => {
+            this.checkForUpdates();
+            console.log('User clicked Update');
+            // Perform the update process here
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  checkForUpdates() {
+    const updateUrl = 'http://192.168.1.42:8080/files/update.xml';
+    const options: AppUpdateOptions = {
+      skipPromptDialog: true,
+      skipProgressDialog: true
+    };
+    
+    this.appUpdate.checkAppUpdate(updateUrl, options).then(update => {
+      console.log(update);
+    }).catch(error => {
+      alert("Error: " + error.msg);
+    });
+  }
 
 }
